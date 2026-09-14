Signed-off-by: Claire Burkett <cab402@francis.edu> Add 1e-6 for h, fix error calculation by using known derivatives of sin and cos.

HW03 #2.py
@@ -7,15 +7,15 @@ def f1(x):          #define f(x)
     return np.cos(x)
 def f2(x):
     return np.sin(x)
-h_arr = [.1,.01,.001,.0001,.00001] #will be used in for loop to test array of h values
+h_arr = [.1,.01,.001,.0001,.00001,.000001] #will be used in for loop to test array of h values
 error1_arr = []     #error calculations will be added to this array anf later plotted
 error2_arr = []
 for h in h_arr:
-    error1 = deriv(f1, x, h) - (-0.21*0.1*np.tanh(0.1*30))    #calculate error
+    error1 = deriv(f1, x, h) - (-np.sin(x))    #estimate deriv. Subtract actual deriv. Error
     error1 = abs(error1)             
-    error1_arr.append(error1)          #Add calculations to list
+    error1_arr.append(error1)          #Add errors to list
 for h in h_arr:
-    error2 = deriv(f2, x, h) - (-0.21*0.1*np.tanh(0.1*30))    #calculate error
+    error2 = deriv(f2, x, h) - (np.cos(x))   #calculate error
     error2 = abs(error2)             
     error2_arr.append(error2)          #Add calculations to list
 # plt.plot(deriv(f1,x,h), color="red")               #plot derivative of cos(x)