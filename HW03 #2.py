f1ab20f1dfa22d90725cac02aa8833bcf371552a
jbandstra <jbandstra@francis.edu>
2026-09-16 11:49:03 -0400

    Instructore feedback on HW03

--- HW03 #2.py
+++ HW03 #2.py
@@ -2,6 +2,12 @@ import numpy as np
 import matplotlib.pyplot as plt
 x=np.linspace(0,10,100)
 def deriv(f,x,h):    #define the derivative as...
+    '''
+    JZB Comment: 
+    There is an issue in the (f(x-h) - f(x+h)) / 2*h
+    This causes the code to divide by 2 and then multiply by h.
+    You want to do /(2*h) or /2/h.
+    '''
     return ( (f(x-h) - f(x+h)) / 2*h )
 def f1(x):          #define f(x)
     return np.cos(x)
@@ -18,8 +24,24 @@ for h in h_arr:
     error2 = deriv(f2, x, h) - (np.cos(x))   #calculate error
     error2 = abs(error2)             
     error2_arr.append(error2)          #Add calculations to list
+'''
+JZB Note:
+You can create multiple plots using plt.figure() when you want to start a new plot.
+'''
 # plt.plot(deriv(f1,x,h), color="red")               #plot derivative of cos(x)
 # plt.plot(deriv(f2,x,h), color= "blue")             #plot derivative of sin(x)
+'''
+JZB Comment:
+We want fix the plotting here.
+There appear to be a couple of issues.
+1. h is just a value. So you want to plot against h_arr
+2. error1_arr and error2_arr are 2-D arrays because x has been defined by a np.linspace command
+The fix for (1) is easy. Just change h to h_arr.
+The fix for (2) is slightly more involved.
+One option is simply to set x to a particular value rather than an array.
+Another option is to collapse the errors for the multiple values of x down to a single value such as the root-mean-square-error
+I suggest you use the first approach for this assignment. We will use the RMSE in our upcoming work on IVPs.
+'''
 plt.plot(error1_arr, h)           #plot error v.s. step size
 plt.plot(error2_arr, h)
 plt.loglog()                          #plot on log scale
